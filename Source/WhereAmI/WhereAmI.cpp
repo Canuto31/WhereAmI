a4748852161b50607429d7d62213328dddfbaebd
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-22 19:09:59 -0500

    creation of the project

---
+++ Source/WhereAmI/WhereAmI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WhereAmI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WhereAmI, "WhereAmI" );
