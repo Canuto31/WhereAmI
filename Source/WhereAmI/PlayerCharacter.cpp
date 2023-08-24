3b3dbef50999fb9a09bdccf055d8f4e5f376cdea
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-24 17:59:18 -0500

    How to work the print in console and print into the screen

--- Source/WhereAmI/PlayerCharacter.cpp
+++ Source/WhereAmI/PlayerCharacter.cpp
@@ -6,30 +6,31 @@
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Log, TEXT("Hola mundo"));
-	
+	int numero = 3;
+	UE_LOG(LogTemp, Warning, TEXT("Hola mundo %d"), numero); //Print in console
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Yellow, TEXT("Esto es un mensaje"));//Print in the screen
+	}
 }
 
 // Called every frame
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
