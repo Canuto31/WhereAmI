299b7e8b6a49fcea8cf0033de3cedceb68f410d6
canuto31 <jcmt.personal.vg@gmail.com>
2024-10-28 22:09:29 -0500

    Structure within .h for item handling

--- Source/WhereAmI/public/Items/ItemDataAsset.h
+++ Source/WhereAmI/public/Items/ItemDataAsset.h
@@ -9,9 +9,47 @@
 /**
  * 
  */
+UENUM(BlueprintType)
+enum class ETypeObject : uint8
+{
+	Consumibles,
+	Arma,
+	Municion,
+	Key
+};
+
+UENUM(BlueprintType)
+enum class EOptionsDoIt : uint8
+{
+	Consumibles,
+	Arma,
+	Municion,
+	Key
+};
+
+USTRUCT(BlueprintType)
+struct FInputInfoStruct
+{
+	GENERATED_BODY()
+	UItemDataAsset* Info;
+	int quantity;
+};
+
 UCLASS()
 class WHEREAMI_API UItemDataAsset : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ETypeObject ObjectType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> SpawnedEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<EOptionsDoIt> Options;
 };
